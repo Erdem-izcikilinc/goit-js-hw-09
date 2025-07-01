4bbddc2615007af78cce8eb31b969ae76973ea75
Erdem İzcikılınç <izcierdem13@gmail.com>
2025-07-01 15:31:40 +0300

    test 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,12 +11,14 @@ form.addEventListener('input', event => {
     }
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
+
 const savedData = localStorage.getItem('feedback-form-state');
 if (savedData) {
     Object.assign(formData, JSON.parse(savedData));
     if (form.elements.email) form.elements.email.value = formData.email || '';
     if (form.elements.message) form.elements.message.value = formData.message || '';
 }
+
 form.addEventListener('submit', event => {
     event.preventDefault();
     if (!formData.email || !formData.message) {
